e698b043a15ee422c3d919cc6fc2e6826ef51e88
rbranham <roger.brnhm@gmail.com>
2021-06-02 18:47:16 -0500

    marked old app bar as depreciated

--- lib/shared/admin_app_bar.dart
+++ lib/shared/admin_app_bar.dart
@@ -2,53 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:route_record_admin_portal/screens/home/home.dart';
 import 'package:route_record_admin_portal/services/auth.dart';
 
-class AdminAppBar extends AppBar {
-
-  //final AuthService _auth = AuthService();
-
-  AdminAppBar({Key key, Widget title}) : super(key: key,
-    title: title,
-    elevation: 0.0,
-    actions: <Widget>[
-      TextButton(
-        child: Text("Routes"),
-        onPressed: () {
-          print("go to route");
-          //TODO need build context??
-          // Navigator.push(
-          //   context,
-          //   MaterialPageRoute(builder: (context) => Home(Home.ROUTE)),
-          // );
-        },
-        style: ButtonStyle(foregroundColor: MaterialStateProperty.all<Color>(Colors.white)),
-      ),
-      TextButton(
-        child: Text("Drivers"),
-        onPressed: () {
-          print("go to drivers"); //TODO: refactor with navigation
-        },
-        style: ButtonStyle(foregroundColor: MaterialStateProperty.all<Color>(Colors.white)),
-      ),
-      TextButton(
-        child: Text("schools"),
-        onPressed: () {
-          print("go to schools"); //TODO: refactor with navigation
-        },
-        style: ButtonStyle(foregroundColor: MaterialStateProperty.all<Color>(Colors.white)),
-      ),
-      FlatButton.icon( //TODO: Update button to a non depreciated version
-        icon: Icon(Icons.person),
-        label: Text('logout'),
-        onPressed: () async {
-          await AuthService().signOut();
-        },
-      )
-    ],
-  );
-}
-
-
-///////////////////////////////////////////////////////////////////////////\\\\
 
 class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
   final Widget title;
@@ -116,4 +69,52 @@ class _CustomAppBarState extends State<CustomAppBar>{
       ],
     );
   }
+}
+
+///////////////////////////////////////////////////////////////////////////\\\\
+
+@Deprecated('needed build context')
+class AdminAppBar extends AppBar {
+
+  //final AuthService _auth = AuthService();
+
+  AdminAppBar({Key key, Widget title}) : super(key: key,
+    title: title,
+    elevation: 0.0,
+    actions: <Widget>[
+      TextButton(
+        child: Text("Routes"),
+        onPressed: () {
+          print("go to route");
+          //TODO need build context??
+          // Navigator.push(
+          //   context,
+          //   MaterialPageRoute(builder: (context) => Home(Home.ROUTE)),
+          // );
+        },
+        style: ButtonStyle(foregroundColor: MaterialStateProperty.all<Color>(Colors.white)),
+      ),
+      TextButton(
+        child: Text("Drivers"),
+        onPressed: () {
+          print("go to drivers"); //TODO: refactor with navigation
+        },
+        style: ButtonStyle(foregroundColor: MaterialStateProperty.all<Color>(Colors.white)),
+      ),
+      TextButton(
+        child: Text("schools"),
+        onPressed: () {
+          print("go to schools"); //TODO: refactor with navigation
+        },
+        style: ButtonStyle(foregroundColor: MaterialStateProperty.all<Color>(Colors.white)),
+      ),
+      FlatButton.icon( //TODO: Update button to a non depreciated version
+        icon: Icon(Icons.person),
+        label: Text('logout'),
+        onPressed: () async {
+          await AuthService().signOut();
+        },
+      )
+    ],
+  );
 }
